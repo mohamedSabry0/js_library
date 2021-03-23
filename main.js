7618061e36ee24f01f6c27780b5f4bc0b104365a
Breno Xavier <brenoxav@gmail.com>
2021-03-23 18:57:02 -0400

    Add bookForm and AddBook button

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const btnAddBook = document.querySelector('.btn-add-book');
+const bookForm = document.querySelector('.book-form');
+const btnSaveBook = document.querySelector('.btn-save-book');
+
 let myLibrary = [];
 
 class Book {
@@ -14,14 +18,31 @@ function addBookToLibrary() {
 
 }
 
+
+btnAddBook.addEventListener('click', () => {
+  if (bookForm.style.display === 'none') {
+    bookForm.style.display = 'block';
+  }
+  else {
+    bookForm.style.display = 'none';
+  }
+})
+
 let first_book = new Book('Sapiens', 'Yuval Noah Harari ', 'interesting book', 400, true);
 let second_book = new Book('Sapiens 2', 'Yuval Noah Harari', 'interesting book', 350, false);
 
 myLibrary.push(first_book);
 myLibrary.push(second_book);
 
-function createCard(book) {
+function removeBook(index) {
+  console.log(myLibrary);
+  myLibrary.splice(index, 1);
+  console.log(myLibrary);
+}
+
+function createCard(book, index) {
   let card = document.createElement('div');
+  card.setAttribute('data-index', index);
 
   let title = document.createElement('h2');
   title.textContent = book.title;
@@ -38,19 +59,23 @@ function createCard(book) {
   let read = document.createElement('p');
   read.textContent = book.read;
 
+  let btnRemoveBook = document.createElement('button');
+  btnRemoveBook.textContent = 'Remove Book'
+  btnRemoveBook.addEventListener('click', removeBook(index));
+
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(description);
   card.appendChild(num_of_pages);
   card.appendChild(read);
-
+  card.appendChild(btnRemoveBook);
   return card;
 }
 
 function displayBooks(library) {
   let container = document.querySelector('.container'); 
-  library.forEach(book => {
-      container.appendChild(createCard(book));
+  library.forEach((book, index) => {
+      container.appendChild(createCard(book, index));
     }
   )
 }
